803ce5012b8fd5d4fa4591b64e0b571c7b50e411
AyushBhxrdwaj <ayush.ds321@gmail.com>
2025-01-15 14:48:00 +0530

    logo fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import logo from '../assets/poke.png'
 const nav = () => {
   return (
     <div className='h-28 w-full bg-red-600 flex p-5 gap-10 justify-between items-center'>
-        <img src="src\assets\poke.png" alt="pokemon logo"  className='h-14'/>
+        <img src={logo} alt="pokemon logo"  className='h-14'/>
         <h1 className='text-xl text-white font-bold mr-5'>Let's find PokeMon</h1>
     </div>
   )
